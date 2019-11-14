fa26b6cd9841ec6d2a4f701f04c971dfa7667078
Michael Grandori <mgrandori90@icloud.com>
2019-11-13 21:11:21 -0800

    reorganized page routing for navbar

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import './App.css';
 
 import Home from './screens/Home';
 import CreateSquad from './screens/CreateSquad';
+import CreateMarine from './screens/CreateMarine';
+import AddMembers from './screens/AddMembers'
+
+import BottomNav from './components/BottomNav'
+
 import ShowSquadDetails from './screens/ShowSquadDetails';
 
 
 class App extends Component {
   render() {
     return (
+      <>
+
       <Router>
         <div>
           <Route exact path='/' component={Home} />
           <Route path='/create-squad' component={CreateSquad} />
+          <Route path='/create-marine' component={CreateMarine} />
           <Route path='/show-squad/:id' component={ShowSquadDetails} />
+          <Route path= '/add-members' component={AddMembers} />
+          {/* <Route path = '/view-appointments' component={ViewAppointments} /> */}
         </div>
       </Router>
+
+      <BottomNav/>
+
+      </>
+
     );
   }
 }
