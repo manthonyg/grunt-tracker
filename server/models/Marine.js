dd3b98a405b0d6c1812a1904aa344a575beaca39
MichaelMBP <michaelmbp@tests-MacBook-Pro.local>
2019-11-19 20:02:26 +0900

    updated schema

--- server/models/Marine.js
+++ server/models/Marine.js
@@ -2,44 +2,52 @@
 
 const mongoose = require('mongoose');
 
+const Appointment = new mongoose.Schema({
+    appointment: {
+        type: Date
+    }
+})
+
 const MarineSchema = new mongoose.Schema({
-  first: {
-    type: String,
-    required: true
-  },
-  last: {
-    type: String,
-    required: true
-  },
-  rank: {
-    type: String,
-    required: true
-  },
-  unit: {
-    type: String,
-    required: true,
-    
-  },
-  company: {
-    type: String,
-    required: true,
-    
-  },
-  platoon: {
-    type: String,
-    required: true,
-  },
-  squad: {
-    type: String,
-    required: true,
-  },
-  team: {
-    type: String,
-    required: true,
-  },
-  appointments: {
-    type: Date, default: Date.now
-  }
+    first: {
+        type: String,
+        required: true
+    },
+    last: {
+        type: String,
+        required: true
+    },
+    rank: {
+        type: String,
+        required: true
+    },
+    unit: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: String,
+        required: true
+    },
+    platoon: {
+        type: String,
+        required: true
+    },
+    squad: {
+        type: String,
+        required: true
+    },
+    team: {
+        type: String,
+        required: true
+    },
+    appointments: {
+        type: Array
+    },
+    accounted: {
+        type: Boolean,
+        default: false
+    }
 
 });
 
