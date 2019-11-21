1ffc849e14bf169473d6d52b76bb6eba3659c70a
MichaelMBP <michaelmbp@tests-MacBook-Pro.local>
2019-11-21 20:44:01 +0900

    simplified schema for testing purposes

--- server/models/Marine.js
+++ server/models/Marine.js
@@ -1,52 +1,46 @@
-// models/Marine.js
 
 const mongoose = require('mongoose');
 
-const Appointment = new mongoose.Schema({
-    appointment: {
-        type: Date
-    }
-})
+
 
 const MarineSchema = new mongoose.Schema({
     first: {
         type: String,
-        required: true
+       
     },
     last: {
         type: String,
-        required: true
+       
     },
     rank: {
         type: String,
-        required: true
+      
     },
     unit: {
         type: String,
-        required: true
+       
     },
     company: {
         type: String,
-        required: true
+        
     },
     platoon: {
         type: String,
-        required: true
+       
     },
     squad: {
         type: String,
-        required: true
+        
     },
     team: {
         type: String,
-        required: true
+        
     },
     appointments: {
         type: Array
     },
     accounted: {
-        type: Boolean,
-        default: false
+        type: '',
     }
 
 });
