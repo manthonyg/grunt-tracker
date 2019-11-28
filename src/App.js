8a678dccfd43a4c2ab98317eea6c4fd7fc304bae
MichaelMBP <michaelmbp@tests-MacBook-Pro.local>
2019-11-28 17:34:22 +0900

    Routing changes/navbar withdraws on scroll

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route, useHistory} from 'react-router-dom';
+
 import './App.css';
 import {Container} from 'reactstrap'
 import Home from './screens/Home';
 import CreateSquad from './screens/CreateSquad';
 import CreateMarine from './screens/CreateMarine';
-import AddMembers from './screens/AddMembers'
 
 import BottomNav from './components/BottomNav'
 
 import ShowSquadDetails from './screens/ShowSquadDetails';
 import ShowMarineDetails from './screens/ShowMarineDetails';
 
+const App = () => {
 
-class App extends Component {
-  render() {
-    return (
-   
-<Container fluid>
+  let [pos,
+    setPos] = useState(window.pageYOffset)
+  let [visible,
+    setVisible] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let temp = window.pageYOffset
+      if (window.pageYOffset > 0) {
+        setVisible(pos < temp)
+        setPos(temp)
+      } else {
+        setVisible(false)
+      }
+
+    };
+    window.addEventListener("scroll", handleScroll);
+    return (() => {
+      window.removeEventListener("scroll", handleScroll);
+    })
+  })
+
+  return (
+
+    <Container fluid>
       <Router>
-        
-          <Route exact path='/' component={Home} />
-          <Route path ='/show-squad-details' component={ShowSquadDetails}/>
-          <Route path='/create-squad' component={CreateSquad} />
-          <Route path='/create-marine' component={CreateMarine} />
-          <Route path='/show-squad/:id' component={ShowSquadDetails} />
-          <Route path='/show-marine/:id' component={ShowMarineDetails} />
-          <Route path='/add-members' component={AddMembers} />
-          {/* <Route path = '/view-appointments' component={ViewAppointments} /> */}
-        
+        <Route exact path='/' component={Home}/>
+        <Route path='/show-squad-details' component={ShowSquadDetails}/>
+        <Route path='/create-squad' component={CreateSquad}/>
+        <Route path='/create-marine' component={CreateMarine}/>
+        <Route path='/show-squad/:id' component={ShowSquadDetails}/>
+        <Route path='/show-marine/:id' component={ShowMarineDetails}/>
+        <BottomNav hidden={visible}/>
       </Router>
-      <BottomNav/>
-      </Container>
 
-      
+    </Container>
 
-    
-
-    );
-  }
+  );
 }
 
 export default App;
