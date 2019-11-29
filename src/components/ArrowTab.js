b0ee0bb2810b7153371b2681db1c1a50b01032be
MichaelMBP <michaelmbp@tests-MacBook-Pro.local>
2019-11-29 18:58:58 +0900

    padding for arrow tab component

--- src/components/ArrowTab.js
+++ src/components/ArrowTab.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import Flex from '../components/Flex';
-import { Link } from 'react-router-dom';
-import { Toast, ToastHeader, ToastBody } from 'reactstrap';
+import {Link} from 'react-router-dom';
+import {Toast, ToastHeader, ToastBody} from 'reactstrap';
 
-const Tab = styled.div`
+const Tab = styled.div `
   padding: 2px;
-  margin-top: 5px;
-  overflow: hidden;
-  background-color: #eee;
+  margin: 5px;
+
+
 `;
 
-const Heading = styled.h4`
+const Heading = styled.h4 `
 color: #bbbbbb;
 font-weight: 200;
 font-size: 14px;
 `
 
-const DeleteIcon = styled.div`
+const DeleteIcon = styled.div `
 position: relative;
 width: 100vw;
 left: 0px;
@@ -25,23 +25,30 @@ top: 0px;
 `
 
 function ArrowTab(props) {
-    return (
-      
-        <Toast>
-        <ToastHeader><DeleteIcon><i class="fa fa-remove" onClick={props.onClick} id={props.id}></i></DeleteIcon></ToastHeader>
-            <Flex justifyAround alignCenter>
+  return (
+    <Tab>
+    <Toast>
+      <ToastHeader>
+        <DeleteIcon>
+          <i class="fa fa-remove" onClick={props.onClick} id={props.id}></i>
+        </DeleteIcon>
+      </ToastHeader>
+      <Flex justifyAround alignCenter>
 
-                <ToastBody><Heading>{props.children}</Heading></ToastBody>
-                
-                <Link to={props.link}>
-                    <i className="tabIcon fa fa-chevron-right"  id={props.id} />
-                </Link>
-               
-            </Flex> 
-        
-        </Toast>
-     
-    )
+        <ToastBody>
+          <Heading>{props.children}</Heading>
+        </ToastBody>
+
+        <Link to={props.link}>
+          <i className="tabIcon fa fa-chevron-right" id={props.id}/>
+        </Link>
+
+      </Flex>
+
+    </Toast>
+    </Tab>
+
+  )
 }
 
 export default ArrowTab
