60221e9b43ce6c45f7ad239db3010eda9c1e62df
MichaelMBP <michaelmbp@tests-MacBook-Pro.local>
2019-12-06 16:45:27 +0900

    smaller logo

--- src/components/LogoSmall.js
+++ src/components/LogoSmall.js
@@ -7,13 +7,13 @@ position: relative;
 top: 10px;
 text-align: center;
 padding-bottom: 20px;
-margin-bottom: 50px;
+margin-bottom: 20px;
 `
 
 function LogoSmall() {
     return (
         <LogoWrapper>
-            <img alt='Grunt Tracker' src={GTLogo} style={{width: '100px'}}/>
+            <img alt='Grunt Tracker' src={GTLogo} style={{width: '50px'}}/>
         </LogoWrapper>
     )
 }
