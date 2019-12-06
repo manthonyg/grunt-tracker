63fdaeaa807c1f5c70451e5b9685bc1db281cc12
MichaelMBP <michaelmbp@tests-MacBook-Pro.local>
2019-12-06 16:44:24 +0900

    updated schema

--- server/models/Marine.js
+++ server/models/Marine.js
@@ -4,38 +4,20 @@ const mongoose = require('mongoose');
 
 const MarineSchema = new mongoose.Schema(
     {
-    first: {
-        type: String,
-       
-    },
-    last: {
-        type: String,
-       
-    },
-    rank: {
-        type: String,
-      
-    },
-    unit: {
-        type: String,
-       
-    },
-    company: {
-        type: String,
-        
-    },
-    platoon: {
-        type: String,
-       
-    },
-    squad: {
-        type: String,
-        
-    },
-    team: {
-        type: String,
-        
-    },
+    first: {type: String},
+    last: {type: String},
+    middle: {type: String},
+    rank: {type: String},
+    unit: {type: String},
+    company: {type: String},
+    platoon: {type: String},
+    squad: {type: String},
+    team: {type: String},
+    edipi: {type: String},
+    blood_type: {type: String},
+    religion: {type: String},
+    accountability: {type: Boolean},
+
     appointments: [
         {
         date: String,
@@ -44,20 +26,23 @@ const MarineSchema = new mongoose.Schema(
         time: String
         }
     ], 
+
     weapons: [
         {
         name: String,
         serial: String
         }
     ],
+
     gear: [
         {
         name: String,
         amount: Number
         }
     ],
-    body: 
-        {
+
+    body: {
+        
         pft: [
             {
             score: String,
@@ -69,8 +54,14 @@ const MarineSchema = new mongoose.Schema(
             score: String,
             last_updated: String
             }
-        ],
-     }
+        ]
+            },
+     
+    allergies: [
+        {type: String}
+    ],
+    
+
     });
 
 module.exports = Marine = mongoose.model('marine', MarineSchema);
