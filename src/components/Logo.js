0b25cd8dfee01d086a09d048fd6f741db62a00c6
MichaelMBP <michaelmbp@tests-MacBook-Pro.local>
2020-04-07 22:20:50 +1000

    troubleshooting

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 //Packages
 import styled, { keyframes, css } from "styled-components";
 // Media
-import GTLogo from "../images/gt_logo.png";
+
 import GTBG from "../images/GT_Bg.png";
 import { device } from "../constants/mediaQueries";
 //Global components
@@ -42,7 +42,6 @@ function Logo({ size, inverted }) {
   return (
     <img
       alt="Grunt Tracker"
-      src={GTLogo}
       style={
         inverted
           ? {
