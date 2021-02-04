d4e9ab52a1a638ae5cc3810382147b0ea68bdd7c
manthony <41161951+manthonyg@users.noreply.github.com>
2021-02-04 13:04:25 -0500

    changed base uri

--- src/services/uris.js
+++ src/services/uris.js
@@ -1,2 +1,4 @@
-export const squadURI = `http://localhost:8082/api/squads`
-export const marineURI = `http://localhost:8082/api/marines`
+// export const squadURI = `http://localhost:8082/api/squads`
+// export const marineURI = `http://localhost:8082/api/marines`
+export const squadURI = `http://ec2-34-215-9-101.us-west-2.compute.amazonaws.com:8082/api/squads`;
+export const marineURI = `http://ec2-34-215-9-101.us-west-2.compute.amazonaws.com:8082/api/marines`;
