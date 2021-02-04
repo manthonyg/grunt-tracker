7c04491743f342fee24d22db0f959682c3bd756f
manthony <41161951+manthonyg@users.noreply.github.com>
2021-02-04 13:50:52 -0500

    prod build

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var cors = require("cors");
 // routes
 const marines = require("./routes/api/marines");
 const squads = require("./routes/api/squads");
+const path = require("path");
 
 //define global vars
 const app = express();
@@ -14,6 +15,8 @@ const port = process.env.PORT || 8082;
 
 // Connect Database
 connectDB();
+
+app.use(express.static(path.join(__dirname, "build")));
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
@@ -26,7 +29,7 @@ app.use("/api/marines", marines);
 app.use("/api/squads", squads);
 
 app.get("/", (req, res) => {
-  res.send("HEY!");
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
